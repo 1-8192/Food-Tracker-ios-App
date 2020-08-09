9f43567f24675f83dc6be9d8cb9a7f545d1f0024
1-8192 <allegranzia@gmail.com>
2020-08-09 16:34:19 -0400

    added image to app

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +32,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
 //Mark: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+    }
     @IBAction func setDefaultLabelTest(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
