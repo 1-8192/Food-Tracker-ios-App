e285d26ba2afeb199039156aee74583a63a6ef44
1-8192 <allegranzia@gmail.com>
2020-08-15 15:23:07 -0400

    added button stack view

---
+++ Food Tracker/RatingControl.swift
@@ -0,0 +1,38 @@
+//
+//  RatingControl.swift
+//  Food Tracker
+//
+//  Created by Alessandro Allegranzi on 8/15/20.
+//  Copyright © 2020 Alessandro Allegranzi. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        //create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+}
