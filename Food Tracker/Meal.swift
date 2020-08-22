cd1d2e75d6c71d1aab39cdd826d0da67239f3958
1-8192 <allegranzia@gmail.com>
2020-08-22 16:14:04 -0400

    added Meal class and wrote unit tests

---
+++ Food Tracker/Meal.swift
@@ -0,0 +1,36 @@
+//
+//  Meal.swift
+//  Food Tracker
+//
+//  Created by Alessandro Allegranzi on 8/22/20.
+//  Copyright © 2020 Alessandro Allegranzi. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+         
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+    }
+}
