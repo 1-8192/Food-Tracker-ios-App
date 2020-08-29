613ddb9522b38501d7b3fadb1ccf7a1796951d6c
1-8192 <allegranzia@gmail.com>
2020-08-29 16:37:15 -0400

    implementing table view

---
+++ Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  Food Tracker
+//
+//  Created by Alessandro Allegranzi on 8/29/20.
+//  Copyright © 2020 Alessandro Allegranzi. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
